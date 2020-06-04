4a72ed5ea7873bf1d9efd8e362c152529416d956
hunter <ulsh.hunter.2015@gmail.com>
2020-06-04 16:14:00 -0700

    change of files with comments

--- selectionsort1.py
+++ selectionsort1.py
@@ -1,32 +1,35 @@
- 
  #make an array for sorting selction
-
  array = [13,4,9,5,3,16,12]
 
+#defines function selectionsort for array
  def SelectionSort(array):
 
+ 	#range = checks length of array
  	n = len(array)
 
+ 	#for var i (minimum value) of range(defined above)
  	for i in range(n): #<-- whatever the len/however many times youll run the for loop
 
  		#initally assume first element of unsorted part as minimum.
 
  		minimum = i 
-
- 		for j in range(i+1.n):
+ 		#j = number in array (minimum+1,calls len of array)
+ 		for j in range(i+1,n):
 
  			if (array[j] < array[minimum]):
 
  			minimum = j
 
 
-
+ 		#instantiates a temporary value for minimum in array
  		temp = array[i]
+ 		#recalls instantiates the i as array minimum
  		array[i] = array minimum
+ 		#recalls minimum in array is temporary
  		array[minimum] = temp
 
+ 	#returns the array after numbers have been sorted
  	return array
- 	
- print(SelectionSort(array))
-
-
+ 
+ #prints the result
+ print(SelectionSort)
