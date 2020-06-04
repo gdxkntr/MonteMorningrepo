2c7b576e0f257748de5f31d02df448cd201e007f
hunter <ulsh.hunter.2015@gmail.com>
2020-06-04 09:29:24 -0700

    changes to file

--- selectionsort1.py
+++ selectionsort1.py
@@ -1,1 +1,32 @@
  
+ #make an array for sorting selction
+
+ array = [13,4,9,5,3,16,12]
+
+ def SelectionSort(array):
+
+ 	n = len(array)
+
+ 	for i in range(n): #<-- whatever the len/however many times youll run the for loop
+
+ 		#initally assume first element of unsorted part as minimum.
+
+ 		minimum = i 
+
+ 		for j in range(i+1.n):
+
+ 			if (array[j] < array[minimum]):
+
+ 			minimum = j
+
+
+
+ 		temp = array[i]
+ 		array[i] = array minimum
+ 		array[minimum] = temp
+
+ 	return array
+ 	
+ print(SelectionSort(array))
+
+
